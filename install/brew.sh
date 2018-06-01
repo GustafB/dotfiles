521114a8bb4528b20dd9519d344fed0c3180821f
GustafN <gustaf.brostedt@gmail.com>
2018-06-01 00:27:58 -0400

    Add auto install for brew and cask

---
+++ install/brew.sh
@@ -0,0 +1,43 @@
+if ! is-macos -o ! is-executable ruby -o ! is-executable curl -o ! is-executable git; then
+  echo "Skipped: Homebrew (missing: ruby, curl and/or git)"
+  return
+fi
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew tap Goles/battery
+brew update
+brew upgrade
+
+# Install packages
+
+apps=(
+  bash-completion2
+  battery
+  coreutils
+  diff-so-fancy
+  gifsicle
+  git
+  git-extras
+  gnu-sed --with-default-names
+  grep --with-default-names
+  httpie
+  psgrep
+  python
+  shellcheck
+  ssh-copy-id
+  tree
+  unar
+  wget
+  wifi-password
+  mongodb
+  postgresql
+  mysql
+)
+
+brew install "${apps[@]}"
+
+export DOTFILES_BREW_PREFIX_COREUTILS=`brew --prefix coreutils`
+set-config "DOTFILES_BREW_PREFIX_COREUTILS" "$DOTFILES_BREW_PREFIX_COREUTILS" "$DOTFILES_CACHE"
+
+ln -sfv "$DOTFILES_DIR/etc/mackup/.mackup.cfg" ~
---
+++ install/cask.sh
@@ -0,0 +1,37 @@
+if ! is-macos -o ! is-executable brew; then
+  echo "Skipped: Homebrew-Cask"
+  return
+fi
+
+brew tap caskroom/versions
+brew tap caskroom/cask
+brew tap caskroom/fonts
+
+# Install packages
+
+apps=(
+  dash2
+  dropbox
+  firefox
+  flux
+  google-chrome
+  google-chrome-canary
+  macdown
+  screenflow
+  slack
+  sourcetree
+  spotify
+  sublime-text
+  transmit
+  virtualbox
+  vlc
+  zeplin
+)
+
+brew cask install "${apps[@]}"
+
+# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook suspicious-package qlvideo
+
+# Link Hammerspoon config
+if [ ! -d ~/.hammerspoon ]; then ln -sfv "$DOTFILES_DIR/etc/hammerspoon/" ~/.hammerspoon; fi
