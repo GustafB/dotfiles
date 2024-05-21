7618432c6a9b28b313dd8ce2bff77218e28135f9
Gustaf Brostedt <gustaf@saporo.io>
2024-05-21 04:53:23 -0600

    track changes in log file

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 script_dir() {
-        if [ -z "${SCRIPT_DIR}" ]; then
-                # even resolves symlinks, see
-                # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
-                    local SOURCE="${BASH_SOURCE[0]}"
-        while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
-                SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    if [ -z "${SCRIPT_DIR}" ]; then
+        # Even resolves symlinks, see
+        # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+        local SOURCE="${BASH_SOURCE[0]}"
+        while [ -h "$SOURCE" ]; do SOURCE="$(readlink "$SOURCE")"; done
+        SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
     fi
-            echo "${SCRIPT_DIR}"
+    echo "${SCRIPT_DIR}"
 }
 
 CODE_HOME=$(script_dir)
@@ -17,9 +17,9 @@ setup_file_link() {
     local source="$1"
     local destination="$2"
     if [ ! -e "${destination}" ]; then
-        ln -nfs "${source}" "${destination}" &&  echo "Linked file ${source} to ${destination}"
+        ln -nfs "${source}" "${destination}" && echo "Linked file ${source} to ${destination}" >> "$LOG_FILE"
     else
-        echo "ERROR: File ${source} already exists"
+        echo "ERROR: File ${destination} already exists" >> "$LOG_FILE"
     fi
 }
 
@@ -27,19 +27,22 @@ setup_dir_link() {
     local source="$1"
     local destination="$2"
     if [ ! -d "${destination}" ]; then
-        ln -s "${source}" "${destination}" && echo "Linked directory ${source} to ${destination}"
+        ln -s "${source}" "${destination}" && echo "Linked directory ${source} to ${destination}" >> "$LOG_FILE"
     else
-        echo "ERROR: Directory ${source} already exists"
+        echo "ERROR: Directory ${destination} already exists" >> "$LOG_FILE"
     fi
 }
 
-# create backup dir if it doesn't exist
+LOG_FILE="${CODE_HOME}/install/install.log"
+echo "Installation started at $(date)" > "$LOG_FILE"
+
+# Create backup dir if it doesn't exist
 if [ ! -d "${HOME}/.backup" ]; then
-  echo "Creating ~/.backup directory"
-  mkdir "${HOME}/.backup"
+    echo "Creating ~/.backup directory" >> "$LOG_FILE"
+    mkdir "${HOME}/.backup"
 fi
 
-# setup links
+# Setup links
 setup_file_link "${CODE_HOME}/zshrc" "${HOME}/.zshrc"
 setup_file_link "${CODE_HOME}/zshenv" "${HOME}/.zshenv"
 setup_file_link "${CODE_HOME}/inputrc" "${HOME}/.inputrc"
@@ -49,20 +52,37 @@ setup_dir_link "${CODE_HOME}/dotemacs.d" "${HOME}/.emacs.d"
 setup_dir_link "${CODE_HOME}/bin" "${HOME}/bin"
 setup_dir_link "${CODE_HOME}/config" "${HOME}/.config"
 
+run_script() {
+    local script_path=$1
+    local script_name=$(basename "$script_path")
+
+    echo "Running $script_name..."
+    echo "Running $script_name..." >> "$LOG_FILE"
+
+    if . "$script_path" >> "$LOG_FILE" 2>&1; then
+        echo "$script_name executed successfully." >> "$LOG_FILE"
+        echo "$script_name executed successfully."
+    else
+        echo "$script_name failed to execute." >> "$LOG_FILE"
+        echo "$script_name failed to execute."
+    fi
+}
 
 if [ "$(uname)" == "Linux" ]; then
-    . "${CODE_HOME}/install/dev-tools.sh"
-    . "${CODE_HOME}/install/python.sh"
-    . "${CODE_HOME}/install/clang17.sh"
-    . "${CODE_HOME}/install/clangd.sh"
-    . "${CODE_HOME}/install/cmake.sh"
-    . "${CODE_HOME}/install/golang.sh"
-    . "${CODE_HOME}/install/zsh.sh"
-    . "${CODE_HOME}/install/ohmyzsh.sh"
-    . "${CODE_HOME}/install/pure.sh"
-    . "${CODE_HOME}/install/gtk-3.sh"
-    . "${CODE_HOME}/install/ranger.sh"
-    . "${CODE_HOME}/install/nvm.sh"
-    . "${CODE_HOME}/install/lua.sh"
-    . "${CODE_HOME}/install/nvim.sh"
+    run_script "${CODE_HOME}/install/dev-tools.sh"
+    run_script "${CODE_HOME}/install/python.sh"
+    run_script "${CODE_HOME}/install/clang17.sh"
+    run_script "${CODE_HOME}/install/clangd.sh"
+    run_script "${CODE_HOME}/install/cmake.sh"
+    run_script "${CODE_HOME}/install/golang.sh"
+    run_script "${CODE_HOME}/install/zsh.sh"
+    run_script "${CODE_HOME}/install/ohmyzsh.sh"
+    run_script "${CODE_HOME}/install/pure.sh"
+    run_script "${CODE_HOME}/install/gtk-3.sh"
+    run_script "${CODE_HOME}/install/ranger.sh"
+    run_script "${CODE_HOME}/install/nvm.sh"
+    run_script "${CODE_HOME}/install/lua.sh"
+    run_script "${CODE_HOME}/install/nvim.sh"
 fi
+
+echo "Installation completed at $(date)" >> "$LOG_FILE"
