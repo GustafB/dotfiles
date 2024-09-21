1b8ec2b89b06390f1c832f9d4c9ce0f21e20cc1b
Gustaf Brostedt <gustaf@saporo.io>
2024-09-21 16:44:08 +0200

    update list-tmux-sessions; add vimwiki

--- config/nvim/lua/plugins/vimwiki.lua
+++ config/nvim/lua/plugins/vimwiki.lua
@@ -1,12 +1,16 @@
 return {
   "vimwiki/vimwiki",
+  event = "BufEnter *.md",
+  keys = { "<leader>ww", "<leader>wt" },
   init = function()
     vim.g.vimwiki_list = {
       {
-        path = "~/docs/vimwiki",
-        syntax = "default",
-        ext = ".wiki",
+        path = "~/docs/vimwiki/",
+        syntax = "markdown",
+        ext = "md",
       },
     }
+    vim.g.vimwiki_ext2syntax = { [".md"] = "markdown", [".markdown"] = "markdown", [".mdown"] = "markdown" }
+    vim.g.vimwiki_global_ext = 0
   end,
 }
