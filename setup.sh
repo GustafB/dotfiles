6a34b0a3d8d05dfcc2919c29421ac92f812f13ba
Gustaf Brostedt <gustaf.brostedt@gmail.com>
2025-09-29 10:17:34 +0200

    add system specific git configs

--- setup.sh
+++ setup.sh
@@ -17,9 +17,9 @@ setup_file_link() {
     local source="$1"
     local destination="$2"
     if [ ! -e "${destination}" ]; then
-        ln -nfs "${source}" "${destination}" && echo "Linked file ${source} to ${destination}" >> "$LOG_FILE"
+        ln -nfs "${source}" "${destination}" && echo "Linked file ${source} to ${destination}" >>"$LOG_FILE"
     else
-        echo "ERROR: File ${destination} already exists" >> "$LOG_FILE"
+        echo "ERROR: File ${destination} already exists" >>"$LOG_FILE"
     fi
 }
 
@@ -27,18 +27,18 @@ setup_dir_link() {
     local source="$1"
     local destination="$2"
     if [ ! -d "${destination}" ]; then
-        ln -s "${source}" "${destination}" && echo "Linked directory ${source} to ${destination}" >> "$LOG_FILE"
+        ln -s "${source}" "${destination}" && echo "Linked directory ${source} to ${destination}" >>"$LOG_FILE"
     else
-        echo "ERROR: Directory ${destination} already exists" >> "$LOG_FILE"
+        echo "ERROR: Directory ${destination} already exists" >>"$LOG_FILE"
     fi
 }
 
 LOG_FILE="${CODE_HOME}/install/install.log"
-echo "Installation started at $(date)" > "$LOG_FILE"
+echo "Installation started at $(date)" >"$LOG_FILE"
 
 # Create backup dir if it doesn't exist
 if [ ! -d "${HOME}/.backup" ]; then
-    echo "Creating ~/.backup directory" >> "$LOG_FILE"
+    echo "Creating ~/.backup directory" >>"$LOG_FILE"
     mkdir "${HOME}/.backup"
 fi
 
@@ -47,6 +47,8 @@ setup_file_link "${CODE_HOME}/zshrc" "${HOME}/.zshrc"
 setup_file_link "${CODE_HOME}/zshenv" "${HOME}/.zshenv"
 setup_file_link "${CODE_HOME}/inputrc" "${HOME}/.inputrc"
 setup_file_link "${CODE_HOME}/gitconfig" "${HOME}/.gitconfig"
+setup_file_link "${CODE_HOME}/gitconfig-macos" "${HOME}/.gitconfig-macos"
+setup_file_link "${CODE_HOME}/gitconfig-wsl" "${HOME}/.gitconfig-wsl"
 setup_file_link "${CODE_HOME}/gitignore_global" "${HOME}/.gitignore_global"
 setup_dir_link "${CODE_HOME}/bin" "${HOME}/bin"
 setup_dir_link "${CODE_HOME}/config" "${HOME}/.config"
@@ -56,16 +58,15 @@ run_script() {
     local script_name=$(basename "$script_path")
 
     echo "Running $script_name..."
-    echo "Running $script_name..." >> "$LOG_FILE"
+    echo "Running $script_name..." >>"$LOG_FILE"
 
-    if . "$script_path" >> "$LOG_FILE" 2>&1; then
-        echo "$script_name executed successfully." >> "$LOG_FILE"
+    if . "$script_path" >>"$LOG_FILE" 2>&1; then
+        echo "$script_name executed successfully." >>"$LOG_FILE"
         echo "$script_name executed successfully."
     else
-        echo "$script_name failed to execute." >> "$LOG_FILE"
+        echo "$script_name failed to execute." >>"$LOG_FILE"
         echo "$script_name failed to execute."
     fi
 }
 
-echo "Installation completed at $(date)" >> "$LOG_FILE"
-
+echo "Installation completed at $(date)" >>"$LOG_FILE"
