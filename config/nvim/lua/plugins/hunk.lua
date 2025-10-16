79739311f348eee4baea8ef8652584795a58af2c
Gustaf Brostedt <gustaf.brostedt@gmail.com>
2025-10-16 09:56:19 +0200

    remove hunk.lua

--- config/nvim/lua/plugins/hunk.lua
+++ config/nvim/lua/plugins/hunk.lua
@@ -1,50 +1,51 @@
-return {
-  {
-    "shellRaining/hlchunk.nvim",
-    enabled = true,
-    -- event = "LazyFile",
-    opts = {
-      chunk = {
-        enable = true,
-        notify = true,
-        use_treesitter = true,
-        chars = {
-          horizontal_line = "─",
-          vertical_line = "│",
-          left_top = "╭",
-          left_bottom = "╰",
-          right_arrow = ">",
-        },
-        style = {
-          { fg = "#a292a3" },
-          { fg = "#c4746e" }, -- this fg is used to highlight wrong chunk
-        },
-        textobject = "",
-        max_file_size = 1024 * 1024,
-        error_sign = true,
-        exclude_filetypes = { neotest_summary = true, ["neo-tree-popup"] = true },
-      },
-      indent = {
-        enable = true,
-        use_treesitter = true,
-        chars = {
-          "│",
-        },
-        style = {
-          { fg = "#2A2A37" },
-        },
-      },
-      line_num = {
-        enable = false,
-      },
-      blank = {
-        enable = false,
-        chars = { " " },
-        style = {
-          { bg = "#2A2A37" },
-          { bg = "", fg = "" },
-        },
-      },
-    },
-  },
-}
+return {}
+-- {
+--   {
+--     "shellRaining/hlchunk.nvim",
+--     enabled = true,
+--     -- event = "LazyFile",
+--     opts = {
+--       chunk = {
+--         enable = true,
+--         notify = true,
+--         use_treesitter = true,
+--         chars = {
+--           horizontal_line = "─",
+--           vertical_line = "│",
+--           left_top = "╭",
+--           left_bottom = "╰",
+--           right_arrow = ">",
+--         },
+--         style = {
+--           { fg = "#a292a3" },
+--           { fg = "#c4746e" }, -- this fg is used to highlight wrong chunk
+--         },
+--         textobject = "",
+--         max_file_size = 1024 * 1024,
+--         error_sign = true,
+--         exclude_filetypes = { neotest_summary = true, ["neo-tree-popup"] = true },
+--       },
+--       indent = {
+--         enable = true,
+--         use_treesitter = true,
+--         chars = {
+--           "│",
+--         },
+--         style = {
+--           { fg = "#2A2A37" },
+--         },
+--       },
+--       line_num = {
+--         enable = false,
+--       },
+--       blank = {
+--         enable = false,
+--         chars = { " " },
+--         style = {
+--           { bg = "#2A2A37" },
+--           { bg = "", fg = "" },
+--         },
+--       },
+--     },
+--   },
+-- }
